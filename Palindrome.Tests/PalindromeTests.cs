66c77700080d2cf6de4a140418b4fe4656b84e7d
josepo <jlpolo@gmail.com>
2022-08-18 20:48:35 +0200

    first version

--- Palindrome.Tests/PalindromeTests.cs
+++ Palindrome.Tests/PalindromeTests.cs
@@ -9,4 +9,28 @@ public class PalindromeTests
 
       Assert.True(word.IsAPalindrome());
    }
+
+   [Fact]
+   public void SameLetterTwiceWord()
+   {
+      var word = "bb";
+
+      Assert.True(word.IsAPalindrome());
+   }
+
+   [Fact]
+   public void LongPalindrome()
+   {
+      var word = "sarabaras";
+
+      Assert.True(word.IsAPalindrome());
+   }
+
+   [Fact]
+   public void NotAPalindrome()
+   {
+      var word = "casimrisac";
+
+      Assert.False(word.IsAPalindrome());
+   }
 }
--- Palindrome/StringExtensions.cs
+++ Palindrome/StringExtensions.cs
@@ -4,6 +4,10 @@ public static class StringExtensions
 {
    public static bool IsAPalindrome(this string word)
    {
+      for (var i = 0; i < word.Length / 2; i++)
+         if (word[i] != word[word.Length - (i + 1)])
+            return false;
+
       return true;
    }
 }
