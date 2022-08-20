70b50b3e240a1a2b14307785be6113d00b5e9e64
josepo <jlpolo@gmail.com>
2022-08-20 19:45:43 +0200

    linq version

--- Palindrome/StringExtensions.cs
+++ Palindrome/StringExtensions.cs
@@ -4,18 +4,8 @@ public static class StringExtensions
 {
    public static bool IsAPalindrome(this string word)
    {
-      int i = 0;
-      int length = word.Length;
-      bool palindrome = true;
+      var reversedWord = new String(word.Reverse().ToArray());
 
-      while (palindrome && i < length / 2)
-      {
-         if (word[i] != word[length - (i + 1)])
-            palindrome = false;
-
-         i++;
-      }
-
-      return palindrome;
+      return word == reversedWord;
    }
 }
